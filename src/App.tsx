2a87b3b8eef0b7b47545ae5dad2ee6004773be22
Miranda-hj <74184412+Miranda-hj@users.noreply.github.com>
2022-03-06 16:58:53 +1300

    completed menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "./App.module.scss";
 import { Content } from "./component/content/content";
+import { Arrow } from "./component/icon/arrow/arrow";
 import { Burger } from "./component/icon/burger/burger";
 import { Navigation } from "./component/navigation/navigation";
 
@@ -8,12 +9,13 @@ export default function App() {
   const [active, setActive] = useState(true);
   return (
     <div className={styles.basic}>
-      <Navigation
-        title={"Register card form"}
-        icon={<Burger />}
-        active={active}
-        onClick={() => setActive(!active)}
-      />
+      <Navigation title={"Register card form"}>
+        {active ? (
+          <Burger onClick={() => setActive(!active)} />
+        ) : (
+          <Arrow onClick={() => setActive(!active)} />
+        )}
+      </Navigation>
       <Content menu={active} />
     </div>
   );
--- src/component/navigation/navigation.tsx
+++ src/component/navigation/navigation.tsx
@@ -1,23 +1,14 @@
 import React from "react";
-import { Arrow } from "../icon/arrow/arrow";
-import { Burger } from "../icon/burger/burger";
 import styles from "./navigation.module.scss";
+
 interface IProps {
   title: string;
-  active: boolean;
-  icon: JSX.Element;
-  onClick: () => void;
 }
-export const Navigation: React.FC<IProps> = ({
-  title,
-  active,
-  icon,
-  onClick,
-}) => {
+
+export const Navigation: React.FC<IProps> = ({ title, children }) => {
   return (
     <div className={styles.basic}>
-      {active ? <Burger /> : <Arrow />}
-      {console.log(active)}
+      {children}
       <h3 className={styles.title}>{title}</h3>
     </div>
   );
