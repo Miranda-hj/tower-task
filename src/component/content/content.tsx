1ac7276a0c887a0428fe833c97f27a6cbaa747d8
Miranda-hj <74184412+Miranda-hj@users.noreply.github.com>
2022-03-06 17:34:15 +1300

    import react form

--- src/component/content/content.tsx
+++ src/component/content/content.tsx
@@ -1,19 +1,23 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import styles from "./content.module.scss";
 
 export const Content = (props: { menu: boolean }) => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
   return (
     <>
       {!props.menu ? (
         <h4 className={styles.menu}>This is menu content</h4>
       ) : (
-        <form method={"get"}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.basic}>
             <h3 className={styles.title}>Welcome </h3>
             <div>
               <input
                 type={"number"}
                 placeholder={"Credit card number"}
+                {...(register("number"), { required: true })}
                 className={styles.cardNumber}
               />
             </div>
@@ -21,18 +25,18 @@ export const Content = (props: { menu: boolean }) => {
               <input
                 type={"number"}
                 placeholder={"CVC"}
+                {...(register("cvc"), { required: true })}
                 className={styles.cvc}
               />
               <input
                 type={"text"}
-                onChange={(e) => console.log(e.target.value)}
                 onFocus={(e) => (e.target.type = "date")}
-                onBlur={(e) => (e.target.type = "text")}
                 placeholder={"Expiry"}
+                {...(register("expiry"), { required: true })}
                 className={styles.expiry}
               />
             </div>
-            <input type={"button"} value={"Submit"} className={styles.submit} />
+            <input type={"submit"} value={"Submit"} className={styles.submit} />
           </div>
         </form>
       )}
