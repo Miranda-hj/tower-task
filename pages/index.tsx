5ecc29ac7ed86bca620cc5ed6e6f33d21390edbc
Miranda-hj <74184412+Miranda-hj@users.noreply.github.com>
2022-03-11 23:56:31 +1300

    fix the cursor bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./index.module.scss";
 import { Content } from "../src/component/content/content";
 import { Navigation } from "../src/component/navigation/navigation";
